c7c6203aa12a099487f88699c2c086b0915ad3ea
kkamara <kamaracomputers@gmail.com>
2025-06-11 16:53:42 +0100

    Emitting Comment Creation Events

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes, } = require("node:crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -12,7 +13,7 @@ app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
     const commentId = randomBytes(4).toString("hex");
     const { content, } = req.body;
 
@@ -21,6 +22,18 @@ app.post("/posts/:id/comments", (req, res) => {
     comments.push({ id: commentId, content, });
 
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post(
+        "http://localhost:4005/events",
+        {
+            type: "CommentCreated",
+            data: {
+                postId: req.params.id,
+                id: commentId,
+                content,
+            }
+        }
+    );
 
     res.status(201).send(comments);
 });
