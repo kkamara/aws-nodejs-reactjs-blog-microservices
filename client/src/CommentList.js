a17a2b3f4116cb82c74a1a0c1c73f3d184398c3e
kkamara <kamaracomputers@gmail.com>
2025-06-11 15:27:49 +0100

    Displaying Comments

---
+++ client/src/CommentList.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState, } from "react";
+import axios from "axios";
+
+function CommentList({ postId, }) {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`,
+    );
+
+    setComments(res.data);
+  };
+
+  const renderedComments = comments.map(comment => {
+    return <li key={comment.id}>{comment.content}</li>;
+  })
+
+  return <ul>
+    {renderedComments}
+  </ul>;
+};
+
+export default CommentList;
