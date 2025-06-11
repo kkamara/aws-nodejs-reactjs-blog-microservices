25eb3f49772e6bc575d88ad6bc3bd26000669336
kkamara <kamaracomputers@gmail.com>
2025-06-11 16:31:02 +0100

    Use built-in ExpressJS body-parser

--- comments/index.js
+++ comments/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const { randomBytes, } = require("node:crypto");
 const cors = require("cors");
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 const commentsByPostId = {};
--- posts/index.js
+++ posts/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const { randomBytes, } = require("node:crypto");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 const posts = {};
