fd4502d93aa4df13f1b272a5b814b280242fd26f
kkamara <kamaracomputers@gmail.com>
2025-06-12 15:08:47 +0100

    Using the Query Service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,21 +1,6 @@
-import React, { useEffect, useState, } from "react";
-import axios from "axios";
+import React from "react";
 
-function CommentList({ postId, }) {
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`,
-    );
-
-    setComments(res.data);
-  };
-
+function CommentList({ comments, }) {
   const renderedComments = comments.map(comment => {
     return <li key={comment.id}>{comment.content}</li>;
   })
