e5578844a4d070ec0151cdc7585d4b6c0ab6aaca
kkamara <kamaracomputers@gmail.com>
2025-06-13 15:15:12 +0100

    Updating Comment Content

--- comments/index.js
+++ comments/index.js
@@ -43,8 +43,40 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Received Event:", req.body.type);
+
+  const { type, data, } = req.body;
+
+  if ("CommentModerated" === type) {
+    const {
+      postId,
+      id,
+      status,
+      content,
+    } = commentsByPostId;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post(
+      "http://localhost:4005",
+      {
+        type: "CommentUpdated",
+        data: {
+          id,
+          status,
+          postId,
+          content,
+        }
+      }
+    )
+  }
+
   res.send({});
 });
 
