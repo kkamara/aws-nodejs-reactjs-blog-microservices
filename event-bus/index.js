ecbad655dccee3dbd9b1888ca4eb1289caabb242
kkamara <kamaracomputers@gmail.com>
2025-06-13 16:23:16 +0100

    Implementing Event Sync

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,8 +4,12 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post(
     "http://localhost:4000/events",
@@ -25,6 +29,10 @@ app.post("/events", (req, res) => {
   );
 
   res.send({ status: "OK", });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
