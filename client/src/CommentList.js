3c22c17a4c4bb2373f10abc072b87e7ec0c86afe
kkamara <kamaracomputers@gmail.com>
2025-06-13 16:01:51 +0100

    Rendering Comments by Status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,21 @@ import React from "react";
 
 function CommentList({ comments, }) {
   const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if ("approved" === comment.status) {
+      content = comment.content;
+    }
+
+    if ("pending" === comment.status) {
+      content = "This comment is awaiting moderation";
+    }
+
+    if ("rejected" === comment.status) {
+      content = "This comment has been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   })
 
   return <ul>
